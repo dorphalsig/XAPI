6f56b6b2cd672dd402839fd22b976abee6e8b0b1
dorphalsig <dorphalsig@gmail.com>
2020-04-12 16:02:01 +0200

    housekeeping

---
+++ index.js
@@ -0,0 +1,4 @@
+import XApiClient from 'src/brokers/xtb/x_api_client';
+import xapi_constants from 'src/brokers/xtb/xapi_constants';
+
+export default {client: XApiClient, constants: xapi_constants};
