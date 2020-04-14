9423a8f11f2d782d6e6d71d058e88f5712e33c22
David Sarmiento <dorphalsig@gmail.com>
2020-04-14 17:55:57 +0200

    code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {XApiClient} from 'brokers/xtb/x_api_client';
 import {Constants} from './brokers/xtb/x_api_constants';
 
-export default {
+export const XApi = {
   Client: XApiClient,
   Constants: Constants,
 };
